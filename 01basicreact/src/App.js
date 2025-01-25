ea68dc401a9b8ba6d9bb9b8cc9aa8d419f8b4340
pritam swami <116485807+Pritam081@users.noreply.github.com>
2025-01-25 13:42:06 +0000

    Commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+import Creat from './creat';
+function App() {
+  return (
+    <Creat />
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/creat.js
@@ -0,0 +1,6 @@
+function Creat(){
+    return (
+        <h1>Chai aur code</h1>
+    )
+}
+export default Creat 
